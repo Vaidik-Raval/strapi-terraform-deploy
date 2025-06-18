7d6826933fd68b15c8e4915485f7c75c18ff4d36
Vaidik-Raval <Vaidik-Raval>
2025-06-18 18:31:12 +0530

    task 7 try

--- terraform/user-data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo yum update -y
-sudo amazon-linux-extras enable docker
-sudo yum install -y docker
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo docker pull vaidikraval5/strapi-app:latest
-sudo docker run -d -p 1337:1337 ${docker_image}
