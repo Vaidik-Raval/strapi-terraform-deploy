d2c84d9d90ccd960e0e338551ab9706410264181
Vaidik-Raval <Vaidik-Raval>
2025-06-19 15:00:14 +0530

    fixed:iam user error

--- terraform/main.tf
+++ terraform/main.tf
@@ -74,7 +74,7 @@ data "aws_iam_role" "ecs_task_exec" {
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task_exec_policy" {
-  role       = aws_iam_role.ecs_task_exec.name
+  role       = data.aws_iam_role.ecs_task_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
