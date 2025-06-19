f9890c5cd7faa19a8c5fccf97271529c6b7c818e
Vaidik-Raval <Vaidik-Raval>
2025-06-19 14:54:28 +0530

    fixed:iam user

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,8 +47,8 @@ resource "aws_ecs_task_definition" "strapi" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecs_task_exec.arn
-  task_role_arn            = aws_iam_role.ecs_task_exec.arn
+  execution_role_arn       = data.aws_iam_role.ecs_task_exec.arn
+  task_role_arn            = data.aws_iam_role.ecs_task_exec.arn
 
   container_definitions = jsonencode([{
     name      = "strapi"
@@ -69,19 +69,8 @@ resource "aws_ecs_task_definition" "strapi" {
   }])
 }
 
-resource "aws_iam_role" "ecs_task_exec" {
+data "aws_iam_role" "ecs_task_exec" {
   name = "ecsTaskExecutionRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action    = "sts:AssumeRole",
-      Effect    = "Allow",
-      Principal = {
-        Service = "ecs-tasks.amazonaws.com"
-      }
-    }]
-  })
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task_exec_policy" {
